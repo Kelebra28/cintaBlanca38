83cde0661e245ecf93083499542482e698b709ff
Kelebra <ricardokelebra@gmail.com>
2020-02-24 20:33:48 -0600

    Condicionales

---
+++ 04_IntroJS/main.js
@@ -0,0 +1,88 @@
+// alert("Hola")
+
+//Valores
+
+//Valor numerico
+var numero = 10;
+// Valor string o texto
+var texto = 'Ricardo';
+//Valor Boolean 
+var verdadero = true;// -> false
+// valor arreglo o array
+var arreglo = [];
+// valor objeto o object
+var objeto = {};
+
+
+var x = 100
+var y = 50
+
+// console.log(x)
+// console.log(y)
+
+// console.log( x + y )
+
+var apellido = 'Basurto'
+
+// console.log(texto);
+// console.log(apellido);
+
+var space = ' '
+var nombreCompleto = texto + space + apellido
+
+// console.log(nombreCompleto)
+
+
+// var pregunta = prompt('Ingresa tu nombre');
+
+// console.log(pregunta);
+
+
+
+// Challege : Preguntar el nombre de mi usiaro, direccion , telfono y en mi consola mostrar la siguiente estructura:
+
+// Tu nombre es : "Nombre"
+// Vive es : "Direccion"
+// Tu telefono es: "Telefono" 
+
+// ----------------
+
+// Preguntar la edad , el año en que nacio y en la consola mostrar los siguiente
+//  Tu edad es : "Edad"
+//Naciste en el año : "año"
+// Por lo tanto el año actual es : " Año actual"
+
+// var nombre  = prompt("Ingresa tu nombre")
+// var direccion  = prompt("Ingresa tu direccion")
+// var telefono = prompt("Ingresa tu telefono")
+// var edad = prompt("Ingresa tu edad")
+// var anio = prompt("Ingresa tu año de nacimiento")
+
+
+// Number es para cambiar el valor de tipo string a numerico
+// var valorEdad = Number(edad)
+// var valorAnio = Number(anio)
+
+// typeof es para saber el valor de mi variable
+// console.log(typeof(valorAnio));
+// console.log(typeof(valorEdad));
+//  var anioActual = valorAnio + valorEdad
+
+// console.log("Tu nombre es: " + nombre);
+// console.log("Tu direccion es : " + direccion);
+// console.log("Tu telefono es : " + telefono);
+// console.log("Tu edad es : " + edad);
+// console.log("El anio en que naciste es : " + anio);
+// console.log("Por lo tanto el año acutal es : " + anioActual);
+
+var cambio = 100
+console.log(cambio);
+
+ cambio = 50
+ console.log(cambio);
+
+ cambio = 'Hola xd'
+ console.log(cambio);
+
+ cambio = true
+ console.log(cambio);
---
+++ 05_Condicionales/main.js
@@ -0,0 +1,46 @@
+// alert('Decarga Ya!!!!!!')
+
+//Estructura de una condicion
+
+// if(condicion){
+//     //pasos a seguir si se cumple
+    // SI DEVUELVE "true"
+// }
+
+//OPERADORES RELACIONALES
+// < > <= >= == === !==
+
+// console.log( 5  < 3);
+
+if( 5 !== '5' ){
+    // console.log('Es verdad :)');
+}else{
+    // console.log('Es falso :C');
+}
+
+
+// preguntar al usuario su edad, evaluarla y decir si es mayor o menor 
+
+
+//Operadores logicos
+
+// AND &&
+
+// if( 10 < 20 && 10 > 5 ){
+//     console.log('Es verdad 🥑');
+// }else{
+//     console.log('Es falso 🚜');
+// }
+
+// OR || 
+
+// if(10 > 20 || 10 < 5){
+//     console.log('Es verdad 🍔');
+// }else{
+//         console.log('Es falso 🍎');
+//     }
+
+
+//CHALLENGE!!!!
+// Juego de piedra pael o tijera
+// P1 y P2 al los cuales debo preguntar que van a elegir
