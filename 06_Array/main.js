966128ddc87b634d6b1d803a7b389d611c4dc2c8
gerardomdn95 <gerardo@devf.mx>
2020-02-25 21:44:45 -0600

    add arrays

---
+++ 06_Array/main.js
@@ -0,0 +1,29 @@
+var nombre = "Gerardo"; // String
+var edad = 24; // Numérico
+var casado = false; // Booleano
+
+// Llaves {}
+// Corchetes []
+// Menor/Mayor que <>
+
+// + sumar
+// - restar
+// * multiplicar
+// / dividir
+
+// Arreglo
+//                0         1         2
+var paises = ["México", "España", "Colombia"];
+console.log(paises[2]);
+console.log(paises[0]);
+console.log(paises[1]);
+
+var arreglo = [true, 3, "Gerardo", 2, "Hola", false];
+console.log(arreglo[3]);
+console.log(arreglo[5]);
+
+var calificaciones = [5, 10, 5, 7, 8, 9];
+var suma = calificaciones[0] + calificaciones[1] + calificaciones[2] + calificaciones[3] + calificaciones[4] + calificaciones[5];
+var promedio = suma / 6;
+console.log(promedio);
+
